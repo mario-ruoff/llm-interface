fa65f4f898a938b2725aac2600e634b3632d850a
Mario <mario.srx99@gmail.com>
2025-12-14 15:14:52 +0100

    Add basic frontend

---
+++ app/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline, Box, Container, TextField } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{
+          display: 'flex',
+          justifyContent: 'center', 
+          alignItems: 'center',
+          minHeight: '100vh',
+        }}>
+        <Container maxWidth="md">
+          <TextField variant='outlined' fullWidth />
+        </Container>
+      </Box>
+    </ThemeProvider>
+  )
+}
+
+export default App
